aa17bfe4b9be36df208ed57300cf4f1ba7a8420c
Rodrigo Meneses <rodrigo.menesesufc@gmail.com>
2021-02-05 11:29:56 -0300

    add binomial_2 and samples

--- trabalho3/rod.py
+++ trabalho3/rod.py
@@ -37,3 +37,47 @@ def binomial(n, p, a, b, m, seed):
             i += 1
         yield i
 
+def binomial_2(n, p, a, b, m, seed):
+    b = bernoulli(p, a, b, m, seed)
+    while (True):
+        yield sum([ next(b) for i in range(n)])
+
+
+
+# Samples
+
+a = 7**5
+b = 0
+m = 2**31 - 1
+seed = 123
+
+# Só pra não ter que botar (a, b, m, seed) em todas as funções,
+# A ideia é que fique bem claro os parâmetros da distribuição
+# Não sei se isso é interessante. No momento fica menos poluido
+
+pr = { "a": a,
+      "b": b,
+      "m": m,
+      "seed": seed }
+
+def sample_unif(size, pr=pr):
+    u = unif(pr['a'], pr['b'], pr['m'], pr['seed'])
+    return [next(u) for i in range(size)]
+
+def sample_bernoulli(p, size, pr=pr):
+    b = bernoulli(p, pr['a'], pr['b'], pr['m'], pr['seed'])
+    return [next(b) for i in range(size)]
+    
+def sample_binomial(n, p, size, pr=pr):
+    b = binomial(n, p, pr['a'], pr['b'], pr['m'], pr['seed'])
+    return [next(b) for i in range(size)]
+    
+def sample_binomial_2(n, p, size, pr=pr):
+    b = binomial_2(n, p, pr['a'], pr['b'], pr['m'], pr['seed'])
+    return [next(b) for i in range(size)]
+    
+print(sample_unif(5))
+print(sample_bernoulli(0.5, 5))
+print(sample_binomial(10,0.5, 5))
+print(sample_binomial_2(10,0.5, 5))
+
