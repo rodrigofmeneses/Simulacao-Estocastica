daa6cd1a6fca5ff183402f06deefcd659c7ad358
Rodrigo Meneses <rodrigo.menesesufc@gmail.com>
2021-02-07 12:09:39 -0300

    update geometric description

--- trabalho3/rod.py
+++ trabalho3/rod.py
@@ -8,19 +8,19 @@
 Geradores de variáveis aleatórias.
 '''
 
-def unif(a, b, m, seed):
+def uniform(a, b, m, seed):
     random = (a * seed + b) % m
     while(True):
         random = (a * random + b) % m
         yield random / m
 
 def bernoulli(p, a, b, m, seed):
-    u = unif(a,b,m,seed)
+    u = uniform(a,b,m,seed)
     while(True):
         yield ( next(u) < p )
 
 def binomial(n, p, a, b, m, seed):
-    u = unif(a,b,m,seed)
+    u = uniform(a,b,m,seed)
     if p >= 1.:
         while True: yield n
     if p <= 0.:
@@ -40,9 +40,24 @@ def binomial(n, p, a, b, m, seed):
 def binomial_2(n, p, a, b, m, seed):
     b = bernoulli(p, a, b, m, seed)
     while True:
-        yield sum([ next(b) for i in range(n)])
+        yield sum([next(b) for i in range(n)])
 
 def geometric(n, p, a, b, m, seed):
+    '''
+        Explicação:
+        next(([iterator]), [default])
+        seja itr = (i for i in range(n)), itr é um iterator,
+        logo o comando next(itr) como já vimos gerará 
+        o próximo elemento desse iterator.
+        
+        Já a expressão (i for i in range(n) if [statment]) é um filtro,
+        logo o iterador só gerará os elementos que passam nesse filtro.
+        
+        Por fim o None, será lançado caso nenhum elemento passe no filtro.
+
+        A distribuição geométrica nos retornará quantas vezes 
+        um lançamento de bernoulli falhou até que ocorra o primeiro sucesso.
+    '''
     b = bernoulli(p, a, b, m, seed)
     while True:
         yield next((i for i in range(n) if next(b)), None)
@@ -53,7 +68,7 @@ def geometric(n, p, a, b, m, seed):
 a = 7**5
 b = 0
 m = 2**31 - 1
-seed = 123
+seed = 5555
 
 # Só pra não ter que botar (a, b, m, seed) em todas as funções,
 # A ideia é que fique bem claro os parâmetros da distribuição
@@ -64,8 +79,8 @@ pr = { "a": a,
       "m": m,
       "seed": seed }
 
-def sample_unif(size, pr=pr):
-    u = unif(pr['a'], pr['b'], pr['m'], pr['seed'])
+def sample_uniform(size, pr=pr):
+    u = uniform(pr['a'], pr['b'], pr['m'], pr['seed'])
     return [next(u) for i in range(size)]
 
 def sample_bernoulli(p, size, pr=pr):
@@ -85,8 +100,8 @@ def sample_geometric(n, p, size, pr=pr):
     return [next(b) for i in range(size)]
 
 
-print("Uniform", sample_unif(5))
+print("uniform", sample_uniform(5))
 print("Bernoulli", sample_bernoulli(0.5, 5))
 print("Binomial", sample_binomial(10,0.5, 5))
 #print(sample_binomial_2(10,0.5, 5))
-print("Geometric", sample_geometric(10, 0.5, 5))
+print("Geometric", sample_geometric(10, 0.2, 5))
