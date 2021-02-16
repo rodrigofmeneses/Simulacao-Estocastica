f65ad2d810def1601c46238d4f25d84da57d1202
rfm <rodrigo.menesesufc@gmail.com>
2021-02-16 11:53:41 -0300

    update sumario.py

--- trabalho3/simulacao/sumario.py
+++ trabalho3/simulacao/sumario.py
@@ -1,4 +1,5 @@
-from simulacao import Random
+from .random import Random
+from math import sqrt
 from scipy import stats
 
 
@@ -8,12 +9,18 @@ class Sumario():
 
     def binomial(self, n=10, p=10, size=1000):
         sample_binomial = rd.binomial(n, p, size)
-        # Valores Obtidos
-        n, (smin, smax), sm, sv, ss, sk = stats.describe(sample_binomial)
-        
         # Valores Esperados
-        mean_esp = n * p
-        meadian_esp = n * p * (1 - p)
+        #esp = [mean_esp, variance_esp, skewness_esp, kurtosis_esp]
+        esp = stats.binom.stats(n, p, moments='mvsk')
+
+        # Valores Observados
+        # obs = [mean_obs, variance_obs, skewness_obs, kurtosis_obs]
+        
+        _, _, mean_obs, variance_obs, skewness_obs, kurtosis_obs = stats.describe(sample_binomial)
+        obs = [mean_obs, variance_obs, skewness_obs, kurtosis_obs]
+
+        self.gerar_tabela(esp, obs)
+        self.gerar_grafico(esp, obs)
 
     
     def gerar_tabela(self, esp, obs):
