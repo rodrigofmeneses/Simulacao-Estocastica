537ae33d22821d887d66c439736c5b9f5d9b2dac
Leonardo de Abreu <leonardo.abreu@lia.ufc.br>
2021-02-16 12:26:44 -0300

    added sumario.[triangular,geometric,weibull]

--- trabalho3/simulacao/sumario.py
+++ trabalho3/simulacao/sumario.py
@@ -22,7 +22,58 @@ class Sumario():
         self.gerar_tabela(esp, obs)
         self.gerar_grafico(esp, obs)
 
-    
+    def triangular(self, lower=0, upper=1, mode=0.5, size=1000):
+        sample_triangular = rd.triangular(lower, upper, mode, size)
+        # Valores Esperados
+        #esp = [mean_esp, variance_esp, skewness_esp, kurtosis_esp]
+        esp = stats.triang.stats(c=mode/(upper-lower), \
+                                 loc=lower, \
+                                 scale=(upper-lower), \
+                                 moments='mvsk')
+
+        # Valores Observados
+        # obs = [mean_obs, variance_obs, skewness_obs, kurtosis_obs]
+        
+        _, _, mean_obs, variance_obs, skewness_obs, kurtosis_obs = stats.describe(sample_triangular)
+        obs = [mean_obs, variance_obs, skewness_obs, kurtosis_obs]
+
+        self.gerar_tabela(esp, obs)
+        self.gerar_grafico(esp, obs)
+
+    def geometric(self, p=0.5, size=1000):
+        sample_geometric = rd.geometric(p, size)
+        # Valores Esperados
+        #esp = [mean_esp, variance_esp, skewness_esp, kurtosis_esp]
+        esp = stats.geom.stats(p=p, loc=0, moments='mvsk')
+
+        # Valores Observados
+        # obs = [mean_obs, variance_obs, skewness_obs, kurtosis_obs]
+        
+        _, _, mean_obs, variance_obs, skewness_obs, kurtosis_obs = stats.describe(sample_geometric)
+        obs = [mean_obs, variance_obs, skewness_obs, kurtosis_obs]
+
+        self.gerar_tabela(esp, obs)
+        self.gerar_grafico(esp, obs)
+
+    def weibull(self, alpha=1, beta=1, size=1000):
+        sample_weibull = rd.weibull(alpha, beta, size)
+        # Valores Esperados
+        #esp = [mean_esp, variance_esp, skewness_esp, kurtosis_esp]
+        esp = stats.weibull_min.stats(c=alpha, \
+                                      loc=0, \
+                                      scale=beta, \
+                                      moments='mvsk')
+
+        # Valores Observados
+        # obs = [mean_obs, variance_obs, skewness_obs, kurtosis_obs]
+        
+        _, _, mean_obs, variance_obs, skewness_obs, kurtosis_obs = stats.describe(sample_weibull)
+        obs = [mean_obs, variance_obs, skewness_obs, kurtosis_obs]
+
+        self.gerar_tabela(esp, obs)
+        self.gerar_grafico(esp, obs)
+
+
     def gerar_tabela(self, esp, obs):
         pass
 
