84128e314cccfd29aa75b408e9d7f3bc7bc61e56
rfm <rodrigo.menesesufc@gmail.com>
2021-02-18 12:31:07 -0300

    add table and samples

--- trabalho3/simulacao/sumario.py
+++ trabalho3/simulacao/sumario.py
@@ -1,26 +1,40 @@
+import numpy as np
 from .random import Random
+from beautifultable import BeautifulTable
 from scipy import stats
-
 
 class Sumario():
     def __init__(self):
         self.rd = Random()
 
-    def binomial(self, n=10, p=0.5, size=1000):
-        sample_binomial = self.rd.binomial(n, p, size)
+    def binomial(self, n=10, p=0.5, size=1000, n_samples=10):
         # Valores Esperados
-        #esp = [mean_esp, variance_esp, skewness_esp, kurtosis_esp]
-        esp = stats.binom.stats(n, p, moments='mvsk')
+        #esperado = [mean_esp, variance_esp, skewness_esp, kurtosis_esp]
+        
+        mean_esp, variance_esp, skewness_esp, kurtosis_esp = stats.binom.stats(n, p, moments='mvsk')
+        median_esp = stats.binom.median(n, p)
+        std_esp = stats.binom.std(n, p)
+
+        esperado = ['Teórica', mean_esp, median_esp, variance_esp, std_esp, kurtosis_esp, skewness_esp]
 
         # Valores Observados
-        # obs = [mean_obs, variance_obs, skewness_obs, kurtosis_obs]
+        # obs = [mean_obs, median_obs, variance_obs, std_obs, kurtosis_obs, skewness_obs]
         
-        _, _, mean_obs, variance_obs, skewness_obs, kurtosis_obs = stats.describe(sample_binomial)
-        obs = [mean_obs, variance_obs, skewness_obs, kurtosis_obs]
+        # Amostras
+        observados = []
+        for i in range(n_samples):
+            self.rd.seed = np.random.randint(10000)
+            sample_binomial = self.rd.binomial(n, p, size)
 
-        self.gerar_tabela(esp, obs)
-        self.gerar_grafico(esp, obs)
-        return esp, obs
+            _, _, mean_obs, variance_obs, skewness_obs, kurtosis_obs = stats.describe(sample_binomial)
+            median_obs = stats.median_abs_deviation(sample_binomial)
+            std_obs = np.sqrt(variance_obs)
+            
+            observados.append([self.rd.seed, mean_obs, median_obs, variance_obs, std_obs, kurtosis_obs, skewness_obs])
+
+        self.gerar_tabela(esperado, observados)
+        # self.gerar_grafico(esp, obs)
+        # return esp, obs
 
     def triangular(self, lower=0, upper=1, mode=0.5, size=1000):
         sample_triangular = self.rd.triangular(lower, upper, mode, size)
@@ -77,13 +91,16 @@ class Sumario():
         return esp, obs
 
 
-    def gerar_tabela(self, esp, obs):
-        pass
+    def gerar_tabela(self, esperado, observados, mean_stats=None, n_samples=None):
+        table = BeautifulTable()
+        table.columns.header = ['Amostras', 'mean', 'median', 'variance', 'std', 'kurtosis', 'skewness']
+        table.rows.append(esperado)
+        for row in observados:
+            table.rows.append(row)
+        print(table)
 
     def gerar_grafico(self, esp, obs):
         pass
 
-        
-    
 
         
